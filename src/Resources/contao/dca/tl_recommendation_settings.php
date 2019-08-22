9112f88a0a2fba576381a60e3b0a2e7d791c4dd1
doishub <daniele@oveleon.de>
2019-08-22 09:14:23 +0200

    Color for active stars added in reccomendation settings

--- src/Resources/contao/dca/tl_recommendation_settings.php
+++ src/Resources/contao/dca/tl_recommendation_settings.php
@@ -19,7 +19,7 @@ $GLOBALS['TL_DCA']['tl_recommendation_settings'] = array
 	// Palettes
 	'palettes' => array
 	(
-		'default'                     => '{image_legend},recommendationDefaultImage;'
+		'default'                     => '{recommendation_legend},recommendationDefaultImage,recommendationActiveColor;'
 	),
 
 	// Fields
@@ -30,6 +30,12 @@ $GLOBALS['TL_DCA']['tl_recommendation_settings'] = array
             'label'                   => &$GLOBALS['TL_LANG']['tl_recommendation_settings']['recommendationDefaultImage'],
             'inputType'               => 'fileTree',
             'eval'                    => array('fieldType'=>'radio', 'filesOnly'=>true, 'isGallery'=>true, 'extensions'=>Config::get('validImageTypes'), 'tl_class'=>'clr')
-		)
+		),
+        'recommendationActiveColor' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_recommendation_settings']['recommendationActiveColor'],
+            'inputType'               => 'text',
+            'eval'                    => array('maxlength'=>6, 'multiple'=>true, 'size'=>1, 'colorpicker'=>true, 'isHexColor'=>true, 'decodeEntities'=>true, 'tl_class'=>'w50 wizard'),
+        )
 	)
 );
