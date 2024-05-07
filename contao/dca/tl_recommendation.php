2aa56b1b468c0aff18cc5d94ed33e5431922fdc0
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2024-05-07 16:18:30 +0200

    Allow saving signed integers in time (fixes #30)

--- contao/dca/tl_recommendation.php
+++ contao/dca/tl_recommendation.php
@@ -177,7 +177,7 @@ $GLOBALS['TL_DCA']['tl_recommendation'] = [
             'load_callback' => [
                 [RecommendationListener::class, 'loadTime']
             ],
-            'sql'                     => "int(10) unsigned NOT NULL default 0"
+            'sql'                     => "int(10) NOT NULL default 0"
         ],
         'teaser' => [
             'exclude'                 => true,
